9d116e60d452ca4923eea37f430dce049c2bf010
mmarkham128 <mmarkham128@gmail.com>
2020-09-12 22:36:29 -0500

    mongo db hooked up to the back end
    As of right now the database has a list of "posts" in it (I'll add users in as well when we get everything hooked to the front end. The database succesfully connects to port 3000 via express (went back and used express generator), and if you travel to /api/posts you can see the list of posts from the database. I also added the body-parser and cors to the app.js.

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+const Post = require("../models/posts");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+
+router.get("/api/posts", (req, res, next) => {
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: documents
+    });
+  });
+});
+
+module.exports = router;
