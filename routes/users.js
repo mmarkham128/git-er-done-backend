0018dc9b532623503dbf61e0247753be7196466f
TheFlexxinTexan <61360594+TheFlexxinTexan@users.noreply.github.com>
2020-09-16 19:40:24 -0400

    User model, login code.

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,39 @@
 var express = require('express');
 var router = express.Router();
+const User = require("../models/users")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  User.find().then(documents => {
+    res.status(200).json({
+      message: "Users fetched went well!",
+      users: documents
+    });
+  });
 });
-
-module.exports = router;
 
 
 // Login Page
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.post('/login', function(req, res, next) {
+  models.users
+    .findOne({
+      where: {
+        Username: req.body.username,
+        Password: req.body.password
+      }
+    })
+    .then(user => {
+      if (user) {
+        res.send('Login succeeded!');
+      } else {
+        res.send('Invalid login!');
+      }
+    });
+});
+
+module.exports = router;
