a5181cea82f713652d5e321cfa5e44bd77f57eb5
TheFlexxinTexan <61360594+TheFlexxinTexan@users.noreply.github.com>
2020-09-20 20:24:09 -0400

    JTW Token stuff

---
+++ services/auth.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken');
+const models = require('../models');
+const bcrypt = require('bcryptjs');
+
+var authService = {
+    signUser: function(user){
+        const token = jwt.sign({
+            UserName: user.userName,
+            UserId: user.UserId,
+            Admin: user.Admin
+        }, 'secretkey',
+        {
+            expiresIn: '1h'
+        });
+        return token;
+    },
+    verifyUser: function(token){
+        try{
+            let decoded = jwt.verify(token, 'secretkey')
+            return models.users.findByPk(decoded.UserId);
+        }catch( err) {
+            console.log(err);
+            return null;
+        }
+    },
+    hashPassword: function(plainTextPassword){
+        let salt = bcrypt.genSaltSync(10);
+        let hash = bcrypt.hashSync(plainTextPassword, salt);
+        return hash;
+    },
+    comparePasswords: function(plainTextPassword, hashedPassword){
+        return bcrypt.compareSync(plainTextPassword, hashedPassword);
+    }
+}
+
+module.exports = authService;
