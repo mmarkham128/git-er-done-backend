b0e60d0bab3a99cb38f39d644f3f4e22c08bedf5
mmarkham128 <mmarkham128@gmail.com>
2020-09-23 19:12:38 -0500

    Update posts.js

--- models/posts.js
+++ models/posts.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose');
 
 const postSchema = mongoose.Schema({
-    title: { type: String, required: true},
-    content:{ type: String, required: true}
+    businessName: { type: String, required: true},
+    contactFirstName: { type: String, required: true},
+    contactLastName: { type: String, required: true},
+    contactMainPhoneNumber: { type: String, required: true},
+    contactStreet: { type: String, required: true},
+    contactCity: { type: String, required: true},
+    contactState: { type: String, required: true},
+    contactZip: { type: String, required: true},
+    employeeFirstName: { type: String, required: true},
+    employeeLastName: { type: String, required: true},
+    jobNotes: { type: String, required: true},
+    employeeID: { type: String, required: false},
+    jobCompleted: { type: Boolean, required: false},
+    jobDeleted: { type: Boolean, required: false},
 
 });
 
