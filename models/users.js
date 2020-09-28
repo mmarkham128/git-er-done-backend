e9c3f52b462bdd418b0865537e87df164e0d1111
mmarkham128 <mmarkham128@gmail.com>
2020-09-27 22:12:22 -0500

    we can create people
    we are the men/women... :)

--- models/users.js
+++ models/users.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
-
 const UserSchema = mongoose.Schema({
-    email: { type: String, required: true},
-    password:{ type: String, required: true}
+    email: { type: String, required: false},
+    password:{ type: String, required: true},
+    employeeFirstName: { type: String, required: true},
+    employeeLastName: { type: String, required: true},
+    employeeCellNumber: { type: String, required: true},
+    username: { type: String, required: true},
+    password: { type: String, required: true},
+     admin: { type: Boolean, required: true},
+    employeeID: { type: Number, required: true},
+    id: { type: String, required: true},
 });
 UserSchema.associate = function(models) {
     // associations defined here
 }
-
 // export model user with UserSchema
 module.exports = mongoose.model("User", UserSchema);
