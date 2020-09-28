e533270ff050e63da523c0d7810936076e4987e1
LindseyDarnell29 <52871510+LindseyDarnell29@users.noreply.github.com>
2020-09-27 21:38:43 -0500

    backend signup

--- app.js
+++ app.js
@@ -57,10 +57,10 @@ app.use(function(err, req, res, next) {
 });
 
 // Just in case
-// app.use(function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
+//app.use(function(req, res, next) {
+ // res.header('Access-Control-Allow-Origin', '*');
+ //  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+ // next();
 // });
 
 module.exports = app;
