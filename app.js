2ab4e6dc6de8ae0af2409effe2689f715ac90f85
TheFlexxinTexan <61360594+TheFlexxinTexan@users.noreply.github.com>
2020-09-27 22:19:59 -0400

    Iffy login code. Conneted but not pulling from databse

--- app.js
+++ app.js
@@ -20,12 +20,20 @@ mongoose
   });
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(cors())
+
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-
+  // Just in case
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+  app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -56,11 +64,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-// Just in case
-// app.use(function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
+
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const User = require("../models/users")
+var mongoose = require('mongoose');
+const UserSchema = require("../models/users");
+const user = require('../models/users')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -10,15 +12,19 @@ router.get('/', function(req, res, next) {
 
 // Login Page
 
+router.get('/login', function(req, res, next){
+  res.render('login')
+})
+
 router.get('/login', function(req, res, next) {
   res.render('login');
 });
 
 router.post('/login', function(req, res, next) {
-  models.users
+  user
     .findOne({
       where: {
-        Email: req.body.email,
+        Username: req.body.username,
         Password: req.body.password
       }
     })
@@ -31,4 +37,5 @@ router.post('/login', function(req, res, next) {
     });
 });
 
+ 
 module.exports = router;
