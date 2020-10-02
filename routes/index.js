921cc3bc5613fbd32e39487cddabe5238eaa7856
Konya Ferrell <race4Jesus@gmail.com>
2020-10-02 16:33:19 -0700

    routes - worked on

--- routes/index.js
+++ routes/index.js
@@ -22,15 +22,18 @@ router.get("/api/posts", (req, res, next) => {
 });
 
 //get all posts where job completed is true
-
+//route that takes you to view all posts where jobCompleted is true or false, depending on the query
 router.get('/api/posts/view', function (req,res,next){
+  //query the portion of the DB that holds the jobCompleted info and finds either a value of true or false b/c of it being a boolean
   let jobCompleted= req.query.jobCompleted
   console.log(req.query.jobCompleted)
   
+  //using the Post model to find a post where jobCompleted is equal to the query of either true or false (based on the let above)
   Post.find({
     jobCompleted: jobCompleted
   }
-    
+  
+  //display will be json and display "posts fetched succesfully" on localhost 3000 and posts the information requested (depending on the true or false query)
   ).then( documents => {
     res.status(200).json({
       message: "Posts fetched succesfully!!!!!!",
@@ -41,7 +44,6 @@ router.get('/api/posts/view', function (req,res,next){
 });
 
 //create a new post
-
 router.post("/api/posts", (req, res, next) => {
   const post = new Post({
     businessName: req.body.businessName,
