502bcac2573dff17112d79259aad66cb12206210
LindseyDarnell29 <52871510+LindseyDarnell29@users.noreply.github.com>
2020-10-02 22:40:41 -0500

    updated user for signup - username

--- models/users.js
+++ models/users.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
 const UserSchema = mongoose.Schema({
 
-    email: { type: String, required: false},
     password:{ type: String, required: true},
     employeeFirstName: { type: String, required: true},
     employeeLastName: { type: String, required: true},
     employeeCellNumber: { type: String, required: true},
-    username: { type: String, required: true},
+    username: { type: String, required: true, unique: true},
     admin: { type: Boolean, required: true},
     employeeID: { type: Number, required: true},
     id: { type: String, required: true},
