0f3e7e6420ab3d33fc6556926165f42df3bafd85
TheFlexxinTexan <61360594+TheFlexxinTexan@users.noreply.github.com>
2020-10-05 01:40:09 -0400

    SignUp with password hashing works!

--- models/users.js
+++ models/users.js
@@ -1,18 +1,24 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator")
+
+
 const UserSchema = mongoose.Schema({
 
     employeeFirstName: { type: String, required: true},
     employeeLastName: { type: String, required: true},
     employeeCellNumber: { type: String, required: true},
     username: { type: String, required: true, unique: true},
-    admin: { type: Boolean, required: true},
-    employeeID: { type: Number, required: true},
+    admin: { type: Boolean, required: true, default: false},
+    employeeID: { type: Number, required: false, unique: true},
     password:{ type: String, required: false}
 });
+
+UserSchema.plugin(uniqueValidator)
+
 UserSchema.associate = function(models) {
     // associations defined here
 }
 // export model user with UserSchema
 
-module.exports = mongoose.model("user", UserSchema);
+module.exports = mongoose.model("User", UserSchema);
 
